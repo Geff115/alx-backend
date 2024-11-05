febc92c61589317c6d8423044c420fa61b551179
Geff115 <gabrielnoah129@gmail.com>
2024-11-05 17:48:11 +0100

    Basic Flask App

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This script creates a simple Flask app
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """rendering index.html page"""
+    return render_template("index.html")

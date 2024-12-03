6102f81fa6ff4a4ce11b0176d1643bb4c952a15b
Geff115 <gabrielnoah129@gmail.com>
2024-12-03 16:36:48 +0100

    Update: converted the client.get and client.set methods into promises, making the compatible with async/await operations. I also made the functions asynchronous

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,44 @@
+import { redisClientFactory } from "kue";
+import { createClient } from "redis";
+import { promisify } from "util";
+
+// Initializing a redis client
+const client = createClient();
+
+// Promisifying the Redis get and set methods
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
+// Connect event listener
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Error event listener
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Async function to set a new school value
+async function setNewSchool (schoolName, value) {
+    try {
+      const reply = await setAsync(schoolName, value);
+      console.log(`Reply: ${reply}`);
+    } catch (err) {
+      console.log(`Error setting ${schoolName}: ${err.message}`);
+    }
+}
+
+
+async function displaySchoolValue (schoolName) {
+  try {
+    const reply = await getAsync(schoolName);
+    console.log(`${reply}`);
+  } catch (err) {
+    console.log(`Error fetching value for ${schoolName}: ${err.message}`);
+  }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
